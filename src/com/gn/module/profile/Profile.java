56ccdb0c39a7452adc8b6b1173accd422e6fbe67
Tân <74073952+ngoctan3006@users.noreply.github.com>
2021-12-06 09:59:47 +0700

    Update Profile.java

--- src/com/gn/module/profile/Profile.java
+++ src/com/gn/module/profile/Profile.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) Gleidson Neves da Silveira
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.gn.module.profile;
 
 import com.gn.model.Member;
@@ -26,10 +10,6 @@ import javafx.scene.text.Text;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-/**
- * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
- * Create on  28/11/2018
- */
 public class Profile implements Initializable {
 
     @FXML
