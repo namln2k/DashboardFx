323b23434a1fc4647f76200db17d76989f85a3a6
Tân <74073952+ngoctan3006@users.noreply.github.com>
2021-12-06 09:58:31 +0700

    Update Login.java

--- src/com/gn/module/login/Login.java
+++ src/com/gn/module/login/Login.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) Gleidson Neves da Silveira
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.gn.module.login;
 
 import animatefx.animation.Pulse;
@@ -38,11 +22,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-/**
- * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
- * Create on  22/11/2018
- * Version 1.0.2
- */
 public class Login implements Initializable {
 
     @FXML
